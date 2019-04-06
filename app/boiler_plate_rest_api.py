e23c93a80f3b0b538dd305fbfea9c5fd2f5043e9
anands <anand.sridharan@suncorp.com.au>
2019-04-06 11:32:18 +1100

    New /status endpoint. Hardcoded for now. Added unittest as well

--- app/boiler_plate_rest_api.py
+++ app/boiler_plate_rest_api.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_restful import Resource, Api
+from flask import jsonify
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,8 +11,22 @@ class HelloWorld(Resource):
     def get(self):
         return 'Hello World'
 
+class Status(Resource):
+    def get(self):
+        data = {"myapplication": 
+                   [{
+                    "version": "1.0",
+                    "description": "pre-interview technical test",
+                    "lastcommitsha": "abc57858585"
+                    }]}
+          
+        resp = jsonify(data)
+        resp.status_code = 200
+        return resp
+ 
 # Expose root to call HelloWorld Resource
 api.add_resource(HelloWorld, '/')
+api.add_resource(Status,'/status')
 
 # Support for both importing and standalone
 if __name__ == '__main__':
--- app/tests/test_boiler_plate.py
+++ app/tests/test_boiler_plate.py
@@ -1,5 +1,6 @@
 import boiler_plate_rest_api
 import unittest
+import json
 
 class boiler_plate_testcase(unittest.TestCase):
     """ boilerplate api test case class """
@@ -16,5 +17,15 @@ class boiler_plate_testcase(unittest.TestCase):
         # For now living with this simple hack in unit tests
         self.assertEqual('"Hello World"', res.data.rstrip())
     
+    def test_status_get(self):
+        res = self.client.get("/status")
+        data = {"myapplication":
+                   [{
+                    "version": "1.0",
+                    "description": "pre-interview technical test",
+                    "lastcommitsha": "abc57858585"
+                    }]}
+        self.assertDictEqual(data, json.loads(res.data))
+
 if __name__ == "__main__":
     unittest.main()
